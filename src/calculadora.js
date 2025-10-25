50092ffcaf475e71e75f29be3a1b5c869efc342b
admin <admin@admin.com>
2025-10-25 09:39:04 -0300

    agregué función factorial

--- src/calculadora.js
+++ src/calculadora.js
@@ -51,6 +51,16 @@ class Calculadora {
     return Math.max(...numeros); 
   }
 
+  factorial(n){
+    if (n < 0) {
+        return "Error: El factorial no está definido para números negativos";
+    }
+    if (n === 0 || n === 1) {
+        return 1;
+    }else {
+      return n * this.factorial(n - 1);
+    }
+  }
 }
 
 // Exportar para usar en tests
